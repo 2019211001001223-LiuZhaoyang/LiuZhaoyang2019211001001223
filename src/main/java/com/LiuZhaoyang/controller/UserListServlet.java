0af1bc3ab652a99dd31e1bfec7d2e44a24a9d901
2019211001001223-LiuZhaoyang <80193519+2019211001001223-LiuZhaoyang@users.noreply.github.com>
2021-05-30 22:10:09 +0800

    13HW-Week 13 Commit
    13HW-Week 13 Commit

---
+++ src/main/java/com/LiuZhaoyang/controller/UserListServlet.java
@@ -0,0 +1,30 @@
+package com.LiuZhaoyang.controller;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.Connection;
+
+@WebServlet("/admin/userList")
+public class UserListServlet extends HttpServlet {
+    Connection con=null;
+    public void init() throws ServletException {
+        con=(Connection)getServletContext().getAttribute("con");
+    }
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String path="/WEB-INF/views/admin/userList.jsp";
+        request.getRequestDispatcher(path).forward(request,response);
+    }
+}
